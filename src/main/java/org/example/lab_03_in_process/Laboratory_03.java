2e7f604e10a7d351b261b893d843cc3754363086
AlSidorenko <alek.sidorenko1979@gmail.com>
2020-12-11 15:16:06 +0200

    Laboratory_03.

--- src/main/java/org/example/lab_03_in_process/Laboratory_03.java
+++ src/main/java/org/example/lab_03_in_process/Laboratory_03.java
@@ -1,5 +1,6 @@
 package org.example.lab_03_in_process;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,25 +17,50 @@ import java.util.Scanner;
     Обчислити з точныстю до е та дослідити збігання послідовності для обраного значення х,
     виводячи кожний р-й додаток, та відповідне значення суми:
     sum(k=0, infinity) (-1)^k/(k! * (k+1)!) * (x/2)^(2k+1)
+
+    По первому признаку Лейбница каждый последующий член ряда по абсолютной величине должен быть меньше
+    предыдущего, т.е. для нашего ряда это условие выполняется
+    1/2 > 1/16 > 1/384.
  */
 
 public class Laboratory_03 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double eps = 0.0, s = 0.0, a = 0.5;
 
-        do {
-            System.out.print("eps = ");
-            eps = Double.valueOf(scanner.nextLine());
-        } while (eps > 0.1f || eps <= 0);
+        System.out.print("Please, enter X (X!=0): ");
+        double x = Double.valueOf(scanner.nextLine());
+        System.out.print("Please, enter K [0, infinity]: ");
+        double kMax = Double.valueOf(scanner.nextLine());
 
-        while (a > eps) {
-            s += a;
-            a /= 2;
-        }
-        System.out.printf("eps = %s, sum = %s", eps, s);
+        System.out.printf("Result: %s\n", mathSeries(x, kMax).toString());
     }
 
+    public static ArrayList<Double> mathSeries(double x, double kMax) {
+        ArrayList<Double> doubleArrayList = new ArrayList<>();
+        double res;
+        for (int k = 0; k < kMax; k++) {
+            res = (Math.pow(-1.0, k) / (factorialInt(k) * factorialInt(k + 1.0))) *
+                    (Math.pow((x / 2.0), ((2.0 * k) + 1.0)));
+            doubleArrayList.add(res);
+        }
+        return doubleArrayList;
+    }
 
+    public static int factorialInt(double kMax) {
+        // проверка на отрицательное значение
+        checkNotNegative(kMax);
+
+        int factorial = 1;
+        for (int i = 2; i <= kMax; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+    private static void checkNotNegative(double k) {
+        if (k < 0) {
+            throw new IllegalArgumentException("negative value");
+        }
+    }
 }
