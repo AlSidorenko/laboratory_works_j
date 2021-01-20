0d40f84a7cbe0f4cd8890c9910aecb72e1e358a5
AlSidorenko <alek.sidorenko1979@gmail.com>
2021-01-20 16:37:22 +0200

    Labs_002_DesAnaliseAndAlgorithm Laboratory_01.

--- src/main/java/org/example/III_course/Labs_002_DesignAndAnalysisOfAlgorithms/lab_001/Laboratory_01.java
+++ src/main/java/org/example/III_course/Labs_002_DesignAndAnalysisOfAlgorithms/lab_001/Laboratory_01.java
@@ -1,9 +1,13 @@
 package org.example.III_course.Labs_002_DesignAndAnalysisOfAlgorithms.lab_001;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created on 03.01.2021 19:36.
@@ -14,23 +18,31 @@ import java.util.Random;
  */
 public class Laboratory_01 {
 
-    static int n = 1000;
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please, enter N: ");
+        int n = sc.nextInt();
 
-    public static void main(String[] args) {
         long timeStart = System.currentTimeMillis();
-        int[][] arr = array();
-        //printMatrix(arr);
-        System.out.printf("Multiplication: %s\n", multiplication(arr));
+        int[][] arr = array(n);
+        printMatrix(arr);
+        System.out.printf("Multiplication: %s\n", multiplication(arr, n));
         long timeFinish = System.currentTimeMillis();
 
         Date dt = new Date(timeFinish - timeStart);
         SimpleDateFormat sdf = new SimpleDateFormat("mm:ss:SS");
-        String res = sdf.format(dt);
-        System.out.printf("Time: %s", res);
+        String resTime = sdf.format(dt);
+        System.out.printf("Time: %s", resTime);
 
+        FileWriter fw = new FileWriter("report.txt", true);
+        BufferedWriter bufferWriter = new BufferedWriter(fw);
+        bufferWriter.write("------------------------------------------- \n");
+        bufferWriter.write(String.format("N = %d\t\t\t\t\t\t Time: %s\n", n, resTime));
+        bufferWriter.close();
+        fw.close();
     }
 
-    // Вывод маирици
+    // Print matrix
     public static void printMatrix(int[][] arr) {
         System.out.println("Input array: ");
         for (int i = 0; i < arr.length; i++) {
@@ -41,7 +53,8 @@ public class Laboratory_01 {
         }
     }
 
-    public static int[][] array() {
+    // Create matrix
+    public static int[][] array(int n) {
         int[][] arrTemp = new int[n][n];
 
         Random r = new Random();
@@ -56,7 +69,7 @@ public class Laboratory_01 {
         return arrTemp;
     }
 
-    public static BigInteger multiplication(int[][] arr) {
+    public static BigInteger multiplication(int[][] arr, int n) {
         BigInteger mult = BigInteger.valueOf(1);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
