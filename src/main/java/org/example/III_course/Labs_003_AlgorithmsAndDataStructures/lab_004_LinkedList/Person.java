03884b480ef26f364c8dce5097b511c5ef4e08de
AlSidorenko <alek.sidorenko1979@gmail.com>
2021-02-26 19:59:23 +0200

    Edit Person

--- src/main/java/org/example/III_course/Labs_003_AlgorithmsAndDataStructures/lab_004_LinkedList/Person.java
+++ src/main/java/org/example/III_course/Labs_003_AlgorithmsAndDataStructures/lab_004_LinkedList/Person.java
@@ -18,26 +18,6 @@ class Person {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Person person = (Person) o;
-
-        if (name != null ? !name.equals(person.name) : person.name != null) {
-            return false;
-        }
-        return surname != null ? surname.equals(person.surname) : person.surname == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (surname != null ? surname.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "Person{" +
                 "name='" + name + '\'' +
